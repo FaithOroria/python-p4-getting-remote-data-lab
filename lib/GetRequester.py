d915467789d26107db58ec369ad9db42579d8eee
Faith Achieng <faith.achieng@student.moringaschool.com>
2023-11-20 23:55:36 +0300

    assignment completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,45 @@
-import requests
-import json
+import requests 
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            if response.status_code == 200:
+                return response.text
+            else:
+                print(f"Request failed with status code: {response.status_code}")
+                return None
+        except requests.RequestException as e:
+            print(f"Request failed: {e}")
+            return None
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        if response_body:
+            try:
+                json_data = eval(response_body)  
+                return json_data
+            except Exception as e:
+                print(f"Error loading JSON: {e}")
+        return None
+
+
+def test_get_response():
+    '''get_response_body function returns response.'''
+    URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    JSON_STRING = '[{"name": "Daniel","occupation": "LG Fridge Salesman"},' \
+                  '{"name": "Joe","occupation": "WiFi Guy"},' \
+                  '{"name": "Avi","occupation": "DJ"},' \
+                  '{"name": "Howard","occupation": "Mountain Legend"}]'
+
+    requester = GetRequester(URL)
+    response_body = requester.get_response_body()
+    assert response_body == JSON_STRING
+
+
+if __name__ == "__main__":
+    test_get_response()
+
--- lib/testing/get_requester_test.py
+++ lib/testing/get_requester_test.py
@@ -1,14 +1,32 @@
 from GetRequester import GetRequester
+import json
 class GetRequesterTest:
     '''Class {Classname} in {modulename}.py'''
 URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
 JSON_STRING = b"[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
 CONVERTED_DATA = [{ 'name': 'Daniel', 'occupation' : 'LG Fridge Salesman' }, { 'name': 'Joe', 'occupation': 'WiFi Fixer' }, { 'name': 'Avi', 'occupation': 'DJ' }, { 'name': 'Howard', 'occupation': 'Mountain Legend' }]
 
+import json
+
 def test_get_response():
-        '''get_response_body function returns response.'''
-        requester = GetRequester(URL)
-        assert(requester.get_response_body() == JSON_STRING)
+    '''get_response_body function returns response.'''
+    URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    JSON_STRING = '[{"name": "Daniel","occupation": "LG Fridge Salesman"},' \
+                  '{"name": "Joe","occupation": "WiFi Fixer"},' \
+                  '{"name": "Avi","occupation": "DJ"},' \
+                  '{"name": "Howard","occupation": "Mountain Legend"}]'
+
+    requester = GetRequester(URL)
+    response_body = requester.get_response_body()
+
+    
+    expected_data = json.loads(JSON_STRING)
+    if response_body:
+        obtained_data = json.loads(response_body)
+        assert obtained_data == expected_data
+    else:
+        assert False, "Failed to retrieve response body"
+
 
 def test_load_json():
         '''load_json function returns response.'''
